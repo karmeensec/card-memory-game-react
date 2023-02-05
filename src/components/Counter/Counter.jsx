bf4a444ff12ff91532a011eeff6ee634b03e00b2
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 23:44:11 +0100

    card counter logic

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,19 +2,37 @@ import React from 'react'
 import './Counter.css'
 import { AiOutlinePlus } from 'react-icons/ai';
 import { AiOutlineMinus } from 'react-icons/ai';
+import PropTypes from 'prop-types'
 
-const Counter = ({ cardsCount }) => {
+const step = 2;
 
+const Counter = ({ cardsCount, onClick }) => {
 
+    const incrementCard = function(e) {
+
+        e.preventDefault();
+        const limit = cardsCount + step;
+
+        if (limit <= 60) onClick(limit);
+
+    }
+
+    const decrementCard = function(e) {
+
+        e.preventDefault();
+        const limit = cardsCount - step;
+
+        if (limit >= 2) onClick(limit);
+    }
 
 
   return (
 
     <div className='total'>
         
-            <button className='minus'> <AiOutlineMinus /> </button>
+            <button className='minus' onClick={decrementCard}> <AiOutlineMinus /> </button>
             <span className='total'> {cardsCount} </span>
-            <button className='plus'> <AiOutlinePlus /> </button>
+            <button className='plus' onClick={incrementCard}> <AiOutlinePlus /> </button>
 
     </div>
 
@@ -22,3 +40,10 @@ const Counter = ({ cardsCount }) => {
 }
 
 export default Counter
+
+Counter.propTypes = {
+
+    cardsCount: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
+
+}
