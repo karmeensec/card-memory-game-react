5cbfd641dc1d5498a826042b0dc4083cafc3eb9c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 15:23:58 +0100

    fetch put inside of the function and useEffect hook

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
 const getRandomPage = () => ( Math.round(Math.random() * (10 -1) + 1 ))  // get random 10 pages
@@ -20,15 +20,26 @@ const useGetImages = () => {
         return url;
     
       }
+
+
+      const fetchImages = function() {
+
+        fetch(constructURL(), {
     
+            headers: {
+              Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
+            }
+          
+          })
+
+      }
     
-      fetch(constructURL(), {
-    
-        headers: {
-          Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
-        }
+    useEffect( ()=> {
+
+        fetchImages();
+
+    }, [])
       
-      })
     
 
 }
