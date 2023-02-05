eb37dfb2cd426b14846541f72766286325aa01bc
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 19:36:30 +0100

    naming convention

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const CATEGORIES = ['abstract', 'people', 'animals', 'nature'];
+export const CATEGORIES = ['Abstract', 'People', 'Animals', 'Nature'];
 export const DIFFICULTY = ['easy', 'medium', 'hard', 'very hard'];
 export const INITIAL_CARD_COUNT = 8;
