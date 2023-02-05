0779ff32bc3c02dd12287a029ea861ced7260172
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 15:14:30 +0100

    fetching the results

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Background from './components/Background/Background'
 
+const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
+
+
 function App() {
+
+ 
+
+
+  fetch(constructURL(), {
+
+    headers: {
+      Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
+    }
+  
+  })
+
+
 
   return (
     
