97ee89aea3848cbf6fa284e88a7a6366b6e7ffda
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 17:34:03 +0100

    hook name changed

---
+++ src/hooks/useGetPhotos.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+
+const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
+const getRandomPage = () => ( Math.round(Math.random() * (10 -1) + 1 ))  // get random 10 pages
+
+const useGetPhotos = () => {
+
+    const [photos, setPhotos] = useState([]);
+
+
+    const constructURL = function() {
+
+        let url = new URL(MAIN_URL);
+    
+        url.search = new URLSearchParams( {
+          query: 'nature',
+          orientation: 'portrait',
+          size: 'small',
+          per_page: '2',
+          page: getRandomPage(),
+        } ) 
+    
+        return url;
+    
+      }
+
+
+      const fetchImages = function() {
+
+        fetch(constructURL(), {
+    
+            headers: {
+              Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
+            }
+          
+          }).then(res => res.json()).then(data => setPhotos(data.photos));
+
+      }
+    
+    useEffect( ()=> {
+
+        fetchImages();
+
+    }, [])
+      
+    return photos;
+
+}
+
+export default useGetPhotos
