111ee58107a5c3f9bb69ff5cf118586387f537f3
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 18:11:42 +0100

    first elements and styling

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import styles from './Settings.module.css';
+
 
 const Settings = () => {
 
   return (
 
-    <div>Settings</div>
-    
+    <div className={`${styles.settings} glassmorphism`} >
+
+        <h2>Settings</h2>
+        <h4>Categories: </h4>
+        <div className={`${styles.settings}`}>bsbsbs</div>
+
+    </div>
+
   )
 }
 
