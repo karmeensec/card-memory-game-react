003bb9626ee82c8a9c4769a8c629ea78ed44cdd9
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 01:01:00 +0100

    background animation component

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from './Background.module.css';
+
+const cubes = [...Array(10)];
+
+const Background = () => (
+
+    <div className={styles.area}>
+
+        <ul className={styles.cubes}>
+            {cubes.map((cube, i) => (
+                <li key={i}>{cube}</li>
+            ))}
+        </ul>
+
+    </div>
+);
+
+export default Background;
