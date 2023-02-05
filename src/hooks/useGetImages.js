61094c624ca1f8457259d7bdbdd5d9332565f9c3
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 15:20:13 +0100

    base URL functions moved

--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
+const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
+const getRandomPage = () => ( Math.round(Math.random() * (10 -1) + 1 ))  // get random 10 pages
+
 const useGetImages = () => {
 
-  return (
+    const constructURL = function() {
 
-    <div>useGetImages</div>
+        let url = new URL(MAIN_URL);
     
-  )
+        url.search = new URLSearchParams( {
+          query: 'nature',
+          orientation: 'portrait',
+          size: 'small',
+          per_page: '2',
+          page: getRandomPage(),
+        } ) 
+    
+        return url;
+    
+      }
+    
+    
+      fetch(constructURL(), {
+    
+        headers: {
+          Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
+        }
+      
+      })
+    
+
 }
 
 export default useGetImages
