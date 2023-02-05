7fddff100f56e02d860d26608f485be022994d54
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 19:44:31 +0100

    cheking the categories

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import styles from './RadioButton.module.css';
 
-const RadioButton = ({ name }) => {
+const RadioButton = ({ name, category }) => {
+
+    const isChecked = name === category;    // if both are equal, true - false
 
   return (
 
     <div className={`${styles.radioBox}`}>
 
-        <input type="radio" name={name} id={name} value={name} />
+        <input type="radio" name={name} id={name} value={name} checked={isChecked} />
         <label htmlFor={name}> {name} </label>
 
     </div>
