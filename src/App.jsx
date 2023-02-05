51becc502c8e9f6969e0039d4dc70fcf2defb5e1
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 15:20:39 +0100

    images hook imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Background from './components/Background/Background'
-
-const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
-const getRandomPage = () => ( Math.round(Math.random() * (10 -1) + 1 ))  // get random 10 pages
+import useGetImages from './hooks/useGetImages'
 
 
 function App() {
 
-  const constructURL = function() {
-
-    let url = new URL(MAIN_URL);
-
-    url.search = new URLSearchParams( {
-      query: 'nature',
-      orientation: 'portrait',
-      size: 'small',
-      per_page: '2',
-      page: getRandomPage(),
-    } ) 
-
-    return url;
-
-  }
-
-
-  fetch(constructURL(), {
-
-    headers: {
-      Authorization: import.meta.env.VITE_REACT_API_AUTH_KEY,
-    }
-  
-  })
-
-
+  useGetImages();
 
   return (
     
