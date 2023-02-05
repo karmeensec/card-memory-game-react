df5c78f2c1bf3363a3ea2b7217aefe5f032c7352
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 15:15:23 +0100

    constructor for URL and random page function

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import './App.css'
 import Background from './components/Background/Background'
 
 const MAIN_URL =  'https://api.pexels.com/v1/search?query=nature';
+const getRandomPage = () => ( Math.round(Math.random() * (10 -1) + 1 ))  // get random 10 pages
 
 
 function App() {
 
- 
+  const constructURL = function() {
+
+    let url = new URL(MAIN_URL);
+
+    url.search = new URLSearchParams( {
+      query: 'nature',
+      orientation: 'portrait',
+      size: 'small',
+      per_page: '2',
+      page: getRandomPage(),
+    } ) 
+
+    return url;
+
+  }
 
 
   fetch(constructURL(), {
