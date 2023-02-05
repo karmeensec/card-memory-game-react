97680418c8af068dfce561a50f6683f0d620cb7d
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 23:25:12 +0100

    counter elements

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './Counter.css'
+import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineMinus } from 'react-icons/ai';
 
 const Counter = () => {
 
   return (
 
-    <div>Counter</div>
-    
+    <div className='total'>
+        
+            <button className='minus'> <AiOutlineMinus /> </button>
+            <span className='total'> 10 </span>
+            <button className='plus'> <AiOutlinePlus /> </button>
+
+    </div>
+
   )
 }
 
