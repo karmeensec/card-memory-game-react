73a0f9fe4a119f75a6d6614dfa43bdd19282f81b
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 01:01:38 +0100

    background render

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Background from './components/Background/Background'
 
 function App() {
 
   return (
-    <div >
-     
-    </div>
+    
+    <>
+      
+      <Background />
+    </>
   )
 }
 
