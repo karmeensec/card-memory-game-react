4a1d1b44cf0b597e86258d6af291f01258d5825c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-05 19:36:48 +0100

    initial elements

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import styles from './RadioButton.module.css';
 
-const RadioButton = () => {
+const RadioButton = ({ name }) => {
 
   return (
 
-    <div>RadioButton</div>
-    
+    <div className={`${styles.radioBox}`}>
+
+        <input type="radio" name={name} id={name} value={name} />
+        <label htmlFor={name}> {name} </label>
+
+    </div>
+
   )
 }
 
