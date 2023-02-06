b87f751741f473b0405127c535180c11c59db6fd
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 21:51:54 +0100

    card elements

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Photo from '../Photo/Photo';
+import styles from './Card.module.css';
 
 const Card = ({ card }) => {
 
   return (
 
-    <div> {card.uniqueId} </div>
+    <div className={`${styles.container}`}>
+
+        <div className={`${styles.card}`}>
+            <div className={`${styles.front}`}></div>
+            <div className={`${styles.back}`}>
+                <Photo url={card.url} />
+            </div>
+        </div>
+
+    </div>
 
   )
 }
