fdb98f598ca0c628eacb0d957a90a5dcfb3f2dcf
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 20:55:49 +0100

    adding unique id to the photos function

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -14,8 +14,24 @@ export const getFormedData = function(data) {
 
 }
 
+// Pair every fetched photo
+
 export const getPairedPhotos = function(data) {
 
     return data.reduce((photo, index) => photo.concat(index, index), []);
 
 }
+
+
+// Extend previously fetched photos and add unique id
+
+export const addUniqueIdPhotos = function(data) {
+
+    return data.map((entry, index) => ({
+        ...entry, 
+        uniqueId: index, 
+    }))
+
+}
+
+
