75e9a13bd1cdb193d07b287b2f8cd60cadd3424f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 20:13:39 +0100

    Loader added

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import PacmanLoader from "react-spinners/PacmanLoader";
+import styles from './Loader.module.css';
 
 const Loader = () => {
     
   return (
 
-    <div>Loader</div>
-    
+    <div className={`${styles.loader}`} > <PacmanLoader /> </div>
+
   )
 }
 
