cdb62376e75b3b3e2027cd745b161e0142e07806
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 21:52:24 +0100

    photo elements

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Photo = () => {
+const Photo = ({url}) => {
 
   return (
 
-    <div>Photo</div>
-    
+    <div>
+        <img src={url} alt="" width='200' />
+    </div>
+
   )
 }
 
 export default Photo
+
+
+Photo.propTypes = {
+
+    url: PropTypes.string.isRequired,
+
+}
