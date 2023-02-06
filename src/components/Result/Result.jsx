0105e543f5bd8ed771741b4367c5e2e315ba36fd
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-07 00:46:10 +0100

    result elements

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import styles from './Result.module.css';
 
 const Result = () => {
 
   return (
 
-    <div>Result</div>
-    
+    <div className={`${styles.container} `}>
+
+        <p>You Won!</p>
+        <button className={`${styles.button} `}>Restart Game</button>
+
+    </div>   
+
   )
 }
 
