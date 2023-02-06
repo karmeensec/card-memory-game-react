6917cb47d3289053096363a458914f9253d99781
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 20:13:57 +0100

    Loader used

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useGetPhotos from '../../hooks/useGetPhotos';
+import Loader from '../Loader/Loader';
+
+
 
 const Board = ({ gameOptions }) => {
 
   const photos =  useGetPhotos(gameOptions);
+
+  const [loader, setLoader] = useState(true);
+
   console.log({photos})
+
+
+  useEffect( ()=> {
+
+    if (photos.length > 0) setLoader(false);
+
+  }, [photos])
+
 
   return (
 
-    <div>Board</div>
+    <div> {loader && <Loader /> } </div>
 
   )
 }
