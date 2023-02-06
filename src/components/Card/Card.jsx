34d457a9d142920ec7682592ee0ad03eb004e81f
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 22:02:09 +0100

    onClick function

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import Photo from '../Photo/Photo';
 import styles from './Card.module.css';
 
-const Card = ({ card }) => {
+const Card = ({ card, cardClick }) => {
+
+    const onClick = function() {
+        cardClick(card.uniqueId);
+    }
 
   return (
 
-    <div className={`${styles.container}`}>
+    <div className={`${styles.container}`} onClick = {onClick}>
 
         <div className={`${styles.card}`}>
+
             <div className={`${styles.front}`}></div>
             <div className={`${styles.back}`}>
                 <Photo url={card.url} />
             </div>
+
         </div>
 
     </div>
