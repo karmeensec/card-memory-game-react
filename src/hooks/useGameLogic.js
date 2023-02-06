3f38165464027b66d910daf75ce4937e471f8cd7
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-02-06 20:43:43 +0100

    first steps game logic

--- src/hooks/useGameLogic.js
+++ src/hooks/useGameLogic.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getFormedData } from '../utilities/utilities'
 
-const useGameLogic = () => {
+const useGameLogic = (photos) => {
 
-  return (
 
-    <div>useGameLogic</div>
-    
-  )
+    const makeCards = function() {
+
+        const formData = getFormedData(photos);
+        console.log(formData);
+
+    }
+
+    useEffect( ()=> {
+
+        if (photos.length > 0) makeCards();
+
+    }, [photos])
+  
 }
 
 export default useGameLogic
